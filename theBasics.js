32e5c97f533c965cfc87d508d7529106875654ef
km-st <kwonmyungstone@gmail.com>
2021-03-23 20:32:01 +1100

    commit

---
+++ theBasics.js
@@ -0,0 +1,44 @@
+// Question 1
+const first = 'john';
+const last = 'doe';
+const full = `${first} ${last}`;
+// Question 2
+const number = 4936;
+const ones = number % 10;
+const onesPlace = number % 10;
+const tens = number % 100;
+const tensPlace = number % 100 - ones;
+const hundreds = number % 1000;
+const hundredsPlace = number % 1000 - tens;
+const thousands = number % 10000;
+const thousandsPlace = number % 10000 - hundreds;
+// Question 3
+typeof 'true' === String;
+typeof false === Boolean;
+typeof 1.5 === Number;
+typeof 2 === Number;
+typeof undefined === undefined;
+typeof { foo : 'bar' } === Object;
+
+// Question 4
+// because + concatenates strings
+
+// Question 5
+Number('5') + 10;
+
+// Question 6
+console.log(`The value of 5 + 10 = ${Number('5') + 10}`);
+
+// Question 7
+// no error, will return undefined
+
+// Question 8
+['spot', '']
+
+// Question 9
+
+// Question 10
+
+// Question 11
+
+// Question 12
